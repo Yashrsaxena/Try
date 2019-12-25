77e4e5ea800342b4a2701b08f3977e06b8db8abb
Yash Saxena <30576909+Yashrsaxena@users.noreply.github.com>
2019-12-25 00:16:29 -0800

    Create 2darray+.cpp

---
+++ 2darray+.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cout<<"Enter the number to be boundaried with\n";
+  cin>>n;
+  int v, s=0;
+  int a[2*n-1][2*n-1];
+  for (v=n;v>0;v--)
+  {
+    for (int i = 0;i < 2*n-1; i++)
+    {
+      for (int j = 0; j < 2*n-1; j++)
+      {
+        if ((i == s || i == 2*n-2-s)||(j == s || j == 2*n-2-s))
+        a[i][j] = v;
+      }
+    }
+    s++;
+  }
+  for (int i=0;i<2*n-1;i++)
+  {
+    for (int j=0;j<2*n-1;j++)
+    cout<<a[i][j]<<" ";
+    cout<<endl;
+  }
+}
